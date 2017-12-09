33c606d973c63d870bba194d7484f25db4d55f0e
Mukul <mukul1904@gmail.com>
2017-12-09 22:52:17 +0530

    Update message files

--- messages.js
+++ messages.js
@@ -1,14 +1,10 @@
 'use strict';
 
 module.exports = {
-    'titleMessage': 'Welcome to NodeXperts Javascript Glossary!',
-    'greetingMessage': 'Speak a JavaScript related term to get information about it.',
+    'titleMessage': 'Welcome to Get Latest Eminem Tweet!',
+    'greetingMessage': 'Ask for latest tweet of Eminem.',
     'repromptMessage': 'Sorry, I\'m not able to hear your request. Please repeat!',
-    'continueMessage': 'Do you want to continue?',
     'goodByeMessgae': 'Goodbye.',
     'invalidIntent': 'We\'re sorry, we are not able to response for your request.',
-    'noDataFound': 'We\'re sorry, we don\'t have information in our records for ',
-    'helpMessage': 'NodeXperts Glossary is an skill where you can ask any ' +
-                   'javascript related term like closure, Promises or for loop, switch ' +
-                   'and this skill will fetch description for you.'
+    'helpMessage': 'You can say \'Tell me latest tweet from Eminem\''
 }
